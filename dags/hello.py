287672299e9a9b63eb47431842b7b9867cab8b71
Dheker Labidi <dhekerabidi@gmail.com>
2023-03-09 16:32:28 +0100

    chore: rename dag

--- dags/hello.py
+++ dags/hello.py
@@ -1,17 +1,18 @@
 from datetime import timedelta, datetime
 from airflow.decorators import dag, task
 
+
 @dag(
     schedule=timedelta(minutes=5),
     start_date=datetime.now(),
-    tags=["example"],
+    tags=["test"],
 )
-def simple_dag():
-
+def hello_dag():
     @task()
     def hello_world():
         print("Hello World!")
 
     hello_world()
 
-simple_dag()
+
+hello_dag()
